f4976576c0e6d45888832f6ea9875a8582572cfb
kiavateam <ava@kiava.ir>
2017-09-24 13:23:07 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-bot_token = "Token" --Put You Token Here
+bot_token = "449326416:AAGJWZXTPvAPAV4-p7m9sleead-6_1XRnCk" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 157059515
+sudo_id = 247134702
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
@@ -152,9 +152,9 @@ function create_config( )
 	server_user = string.gsub(server_user, '%s+$', '')
 	server_user = string.gsub(server_user, '[\n\r]+', ' ') 
 	if server_user:match("^root$") then
-		BeyondTeam = '/root/BDReborn'
+		BeyondTeam = '/root/ngyca'
 	elseif not server_user:match("^root$") then
-		BeyondTeam = '/home/'..server_user..'/BDReborn'
+		BeyondTeam = '/home/'..server_user..'/ngyca'
 	end
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
@@ -162,28 +162,11 @@ function create_config( )
     "core",
     "plugins"
     },
-    sudo_users = {157059515},--Sudo users
+    sudo_users = {247134702,291690408},--Sudo users
     admins = {},
     disabled_channels = {},
     moderation = {data = ''..BeyondTeam..'/data/moderation.json'},
-    info_text = [[*》Beyond Helper Bot V1.5*
-`》An helper bot of` *BDReborn* `based on` *BDMessenger*
-
-》[Beyond Helper](https://github.com/BeyondTeam/BDHelper)
-
-*》Admins :*
-*》Founder & Developer :* [SoLiD](Telegram.Me/SoLiD)
-_》Developer :_ [ToOfan](Telegram.Me/ToOfan)
-_》Developer & Sponser :_ [MAKAN](Telegram.Me/MAKAN)
-
-*》Special thanks to :*
-》[Amir Bagheri](Telegram.Me/CodeLua)
-
-*》Our channel :*
-》[BeyondTeam](Telegram.Me/BeyondTeam)
-
-*》Our Site :*
-》[BeyondTeam](BeyondTeam.ir)
+    info_text = [[
 ]],
   }
   serialize_to_file(config, './data/config.lua')
