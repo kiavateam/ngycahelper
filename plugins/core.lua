40c1bb704fb1794830cd93bf07aa7da100428fb0
kiavateam <ava@kiava.ir>
2017-09-24 19:37:15 +0330

    Update core.lua

--- plugins/core.lua
+++ plugins/core.lua
@@ -2684,21 +2684,17 @@ if matches[1] == '/beyond' then
     edit_inline(msg.message_id, text, keyboard)
 end
 
-if matches[1] == '/solid' then
-local text = [[*》SoLiD Information《*
-_》Age :_ *26*
-_》Name :_ *Saeid Rostami*
-_》City :_ *Shahriar - Tehran*
+if matches[1] == '/kiava' then
+local text = [[*》Kiava Information《*
+_》Age :_ *22*
+_》Name :_ *Kiarash Khatib*
+_》City :_ *Iran - Tehran*
 *-------------------------*
-*》GitHub :《*
-》[SoLiD021](Github.Com/SoLiD021)
-*-------------------------*
-*》Bridges :《*
-_》Pv : _[@SoLiD](Telegram.Me/SoLiD)
-_》PvResan : _[@SoLiD Pv](Telegram.Me/SoLiD021Pv_Bot)
+*》Site :《*
+》[KiavaCo.](Https://kiava.co)
 *-------------------------*
 *》Expertise :《*
-_》_*Lua*, *Cli* `and` *Api* _Bots_
+_》_All Of the Languages Programming_
 *-------------------------*]]
   if not lang then
 		keyboard = {} 
@@ -2717,21 +2713,14 @@ _》_*Lua*, *Cli* `and` *Api* _Bots_
   end
     edit_inline(msg.message_id, text, keyboard)
 end
-if matches[1] == '/toofan' then
-local text = [[*》ToOfan Information《*
+if matches[1] == '/ngyca' then
+local text = [[*》Negisa Information《*
 _》Age :_ *35*
-_》Name :_ *Ali AzizAbadi*
-_》City :_ *Bam - Kerman*
+_》Name :_ *negisa*
+_》City :_ *Iran - Gorgan*
 *-------------------------*
-*》GitHub :《*
-》[To0fan](GitHub.Com/To0fan)
-*-------------------------*
-*》Bridges :《*
-_》Pv :_ [@ToOfan](Telegram.Me/ToOfan)
-_》PvResan :_ [@ToOfanRobot](Telegram.Me/ToOfanRobot)
-*-------------------------*
-*》Expertise :《*
-*》C#, Lua, VB.Net, JAVA*
+*》Telegram :《*
+》[Ngyccaw](t.me/Ngyccaw)
 
 *-------------------------*]]
   if not lang then
